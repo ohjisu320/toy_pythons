a77b2a27baa8d5ce8d5c80645fed81635e9519ff
handongchul <hansco75495@gmail.com>
2023-12-06 17:36:03 +0900

    상품 수정

--- docs/polls_first_dongchulhan.py
+++ docs/polls_first_dongchulhan.py
@@ -4,7 +4,7 @@ list_question = [
     "상품의 디자인에 대해 어떻게 생각하시나요?",
     "상품에 대한 전반적인 만족도는 어떠신가요?"
     ]
-list_answer = ["졸음", "중간","좋아지길"]
+list_answer = ["좋음", "중간","좋아지길"]
 for num_question_counts in [1,2,3,4]:
     num_question_counts = num_question_counts - 1
     num_question = num_question_counts + 1
@@ -15,5 +15,6 @@ for num_question_counts in [1,2,3,4]:
         num_answer= num_answer_counts +1
         str_answer = list_answer[num_answer_counts]
         print("{}.{}" .format(num_answer,str_answer), end = "")
+    if num_question_counts != 3:
         print("")
-    print("-----------------")
+        print("-----------------")
