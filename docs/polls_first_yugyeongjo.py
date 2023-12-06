ed59d9f8a7521c100043833bc53a9ec2364b98db
Yugyeong Jo <yugyeongjo1215@gmail.com>
2023-12-06 18:23:53 +0900

    modify

--- docs/polls_first_yugyeongjo.py
+++ docs/polls_first_yugyeongjo.py
@@ -5,19 +5,18 @@ list_question = [
     ,"상품에 대한 전반적인 만족도는 어떠신가요?"
 ]
 list_answer =  ["좋음", "중간", "좋아지길"]
-# for number_count in ["1", "2", "3", "4"]
 
 number = 0
 
-for str_question in list_question:
-    number += 1
+for str_question in list_question:         
+    number += 1                                    # 문항 번호 입력
     print("{}. {}".format(number, str_question))
     pass
-    number_02 = 0
+    number_02 = 0                                  # 답항 번호 입력("1. 2. 3."반복 필요 > 하위 for문 밖으로 배치)
     for str_answer in list_answer:
-        number_02 += 1
+        number_02 += 1                             # 답항 번호 입력
         print("{}. {}".format(number_02, str_answer), end=" ")
-    print("")  
+    print("")                                      # 줄바꿈
     print("-------------------------------")
 
 print("End program!")
