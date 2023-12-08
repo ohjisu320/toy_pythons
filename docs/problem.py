49beff836011c59340028711dadbff81464c773c
handongchul <hansco75495@gmail.com>
2023-12-08 14:22:16 +0900

    add

---
+++ docs/problem.py
@@ -0,0 +1,22 @@
+#question list
+questions_list = [
+                "문제1. Python에서 변수를 선언하는 방법은? (점수: 10점)"
+                ,"문제2. Python에서 리스트(List)의 특징은 무엇인가요? (점수: 15점)"
+                ,"문제3. Python에서 조건문을 작성하는 방법은? (점수: 10점)"
+                ,"문제4. Python에서 함수를 정의하는 방법은? (점수: 5점)"
+                ]
+#answer list
+examples_list = [
+            "1) var name, 2) name = value, 3) set name, 4) name == value"
+            ,"1) 순서가 있고 변경 가능하다, 2) 중복된 값을 가질 수 없다, 3) 원소를 추가하거나 삭제할 수 없다, 4) 정렬된 상태로 유지된다"
+            ,"1) if-else, 2) for-in, 3) while, 4) def"
+            ,"1) class, 2) def, 3) import, 4) return"
+            ]
+
+answer = [2, 1, 1, 2]
+
+for answer_01 in []:
+    print(questions_list[])
+    print(examples_list)
+    print("정답 : {}".format([answer_01]))
+    
